12fd1831c0109703c12e8e400654feeab2f9c1aa
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-12 13:33:22 +0900

    증감 연산자의 사용
    —a 라면 명령이 수행되기 전에 먼저 빠지고
    a++이라면 명령이 수행되고 더해진다.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculation
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a = 10;
+			Console.WriteLine (a++);
+			Console.WriteLine (++a);
+
+			Console.WriteLine (a--);
+			Console.WriteLine (--a);
+		}
+	}
+}
